dc82f251c52f951bd8aebbc809df173d38ffb20c
sonic55567 <sonic55567@gmail.com>
2020-09-17 02:07:34 +0800

    import success

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
+function reformatTA(data) {
+    for(i=0;i<data[sheet].length;i++) {
+        TAArray[0].push(data[sheet][i][key1]);
+        TAArray[1].push(data[sheet][i][key2]);
+    }
+}
+
+function reformatClass(data) {
+    for(i=0;i<data[sheet].length;i++) {
+        classArray[0].push(data[sheet][i][key1]);
+        classArray[1].push(data[sheet][i][key2]);
+    }
+}
+
 function test() {
     if(!TAFile.files[0] || !classFile.files[0]) {
         alert("請先選取檔案");
         return;
     }
-    console.log(TAFile.files[0]['name']);
+}
+
+function parseTime() {
+    
 }
